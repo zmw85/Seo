75fe6aa52e9dd8bae731870bee7dd780eabf1ed7
zmw85 <jeffwang03@gmail.com>
2017-07-05 03:24:21 +1000

    first commit

---
+++ src/SEO.RankReport/Models/SearchHit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEO.RankReport.Models
+{
+    public class SearchHit
+    {
+        public int Index { get; set; }
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ src/SEO.RankReport/wwwroot/jsx/index.jsx
@@ -0,0 +1,43 @@
+﻿var SearchResult = React.createClass({
+    render: function() {
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.hits.map(function(){
+                        <tr>
+                            <td>{index}</td>
+                            <td>{text}</td>
+                            <td>{url}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        );
+    }
+});
+
+var hits = [];
+
+ReactDOM.render(
+  <SearchResult hits={hits} />,
+  document.getElementById('content')
+);
+
+
+$(document).ready(function () {
+    $('#btnSearch').click(function () {
+        $.ajax({
+            url: '/api/search',
+            data: { keyword: $('#txtKeyword').val(), urlPrefix: $('#txtUrlPrefix').val() }
+        }).done(function (res) {
+            hits = res;
+        });
+    });
+});
